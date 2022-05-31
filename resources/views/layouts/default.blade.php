04355611e7f084f2ba2ee90c1759b86b7e36c965
kobe <123456@qq.com>
2022-05-31 12:48:19 +0000

    完成注册

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,8 +7,11 @@
 <body>
 @include('layouts._header')
 <div class="container">
+  <div class="offset-md-1 col-md-10">
+    @include('shared._messages')
 @yield('content')
 @include('layouts._footer')
+  </div>
 </div>
 </body>
 </html>
