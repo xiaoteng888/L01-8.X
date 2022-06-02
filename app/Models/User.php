724847bd1f100ee02b8f1f12fc9fcdd45723449e
kobe <123456@qq.com>
2022-06-02 09:32:39 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,10 @@ class User extends Authenticatable
         });
     }
 
+    public function statuses()
+    {
+        return $this->HasMany(Status::class,'user_id','id');
+    }
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
