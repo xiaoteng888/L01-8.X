9750182752f053a046cc1afdbc5129ee5324e4ec
kobe <123456@qq.com>
2022-06-03 06:37:30 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -117,4 +117,19 @@ class UsersController extends Controller
             $message->to($to)->subject($subject);
         });
     }
+
+    //关注列表
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(10);
+        $title = $user->name . '关注的人';
+        return view('users.show_follow', compact('users','title'));
+    }
+    //粉丝列表
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate('10');
+        $title = $user->name . '的粉丝';
+        return view('users.show_follow',compact('users','title'));
+    }
 }
