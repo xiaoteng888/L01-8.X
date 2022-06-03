bd4939078017960d4b5c67bb99c9d69c34d3d457
kobe <123456@qq.com>
2022-06-03 05:49:40 +0000

    社交统计信息

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
         Model::unguard();
     }
 }
