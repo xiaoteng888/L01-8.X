384e625d45d516a74c9b9e74de9087d1ca4b60a2
kobe <123456@qq.com>
2022-06-03 07:46:36 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,7 +65,10 @@ class User extends Authenticatable
     //取出当前用户所有的微博
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at','desc');
+        //return $this->statuses()->orderBy('created_at','desc');
+        $user_ids = $this->followings()->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+        return Status::whereIn('user_id',$user_ids)->with('user')->orderBy('created_at','desc');
     }
 
     //获取粉丝
